8721a0bca85fe03aff35efb9c837fd4b65ab45b7
ratko <ratko.mkd@outlook.com>
2019-01-15 13:44:59 -0600

    Add camera, take photo,save photo are done. Filters are not...seemed hard at that time

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,54 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+// Ova ovde che pocni da snima vo Browsero od kamerata
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false
+    })
+    .then(localMediaStream => {
+      // console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+// Take frame from this video and paint it to the canvas
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  // link.textContent = 'Download Image';
+  link.innerHTML = `<img src='${data}' alt='Handsome Man' />`;
+  strip.insertBefore(link, strip.firstChild)
+  // console.log(data);
+
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
